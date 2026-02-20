e018561e3dbd157309dba2d91f6bceecfd09fef0
Zone <shisanupong5415@gmail.com>
2026-02-20 17:55:40 +0700

    p1

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,1 +1,22 @@
-export class Car {}
+export class Car {
+    brand: string;
+    model: string;
+
+    constructor() {
+        this.brand = "";
+        this.model = "";
+    }
+    
+    start() {
+        console.log(`${this.brand} ${this.model} is starting...`);
+    }
+    accelerate() {
+        console.log(`${this.brand} ${this.model} is accelerating...`);
+    }
+    brake() {
+        console.log(`${this.brand} ${this.model} is braking...`);
+    }
+    stop() {
+        console.log(`${this.brand} ${this.model} is stopping...`);
+    }
+}
