92431296d10e72892fa7d26a335494ccf37c2330
Zone <shisanupong5415@gmail.com>
2026-02-20 18:08:55 +0700

    p2

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,40 @@
-export class User {}
+export class User {
+    firstname: string;
+    lastname: string
+    private age: number;
+    static BIRTH_YEAR: number ;
+
+    constructor(firstname: string , lastname: string , age: number ) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    set setFirstname(firstname: string) {
+        this.firstname = firstname;
+    }
+
+    get getFirstname(): string {
+        return this.firstname;
+    }
+
+    set setLastname(lastname: string) {
+        this.lastname = lastname;
+    }
+
+    get getLastname(): string {
+        return this.lastname;
+    }
+
+    set setAge(age: number) {
+        this.age = age;
+    }
+
+    get getAge(): number {
+        return this.age;
+    }
+
+    get FullName(): string {
+        return `${this.firstname} ${this.lastname}`;
+    }
+}
