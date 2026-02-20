0aa8a05af86c9f2281e30e8fa3cb5b2776429069
Zone <shisanupong5415@gmail.com>
2026-02-20 18:26:11 +0700

    p03

--- src/practicals/p03/character.ts
+++ src/practicals/p03/character.ts
@@ -1,1 +1,19 @@
-export class Character {}
+export class Character {
+    name: string;
+    health: number;
+
+    constructor(name: string , health: number = 100) {
+        this.name = name;
+        this.health = health;
+    }
+    
+    getName(): string {
+        return this.name;
+    }
+    getHealth(): number {
+        return this.health;
+    }
+    receiveDamage(damage: number): void {
+        this.health -= damage;
+        }
+}
--- src/practicals/p03/warrior.ts
+++ src/practicals/p03/warrior.ts
@@ -1,5 +1,19 @@
 import { Character } from "./character";
 
 export class Warrior extends Character {
-  // TODO: implement class properties, constructor with super(...), and methods
+    weapon: string;
+
+    constructor(name: string , weapon: string , health: number ) {
+        super(name, health);
+        this.weapon = weapon;
+    }
+
+    getWeapon(): string {
+        return this.weapon;
+    }
+
+     override receiveDamage(damage: number): void {
+        const reducedDamage = damage * 0.65;
+        super.receiveDamage(reducedDamage);
+    }
 }
